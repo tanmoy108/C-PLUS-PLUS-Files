3040a1c3a8d48ab07a2ad4f749fb0a9ffb02d250
Sh Tanmoy <62763494+tanmoy108@users.noreply.github.com>
2022-08-25 22:00:18 +0600

    palindrome number.cpp

---
+++ palindrome number.cpp
@@ -0,0 +1,58 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int row;
+    cout<<"row: ";
+    cin>>row;
+    for(int i=1; i<=row; i++)
+    {
+        for(int j=1; j<=row-i; j++)
+        {
+            cout<<"  ";
+        }
+        for(int k =i ; k>=1; k--)
+        {
+            cout<<k<<" ";
+        }
+        for(int l =1 ; l<=i; l++)
+        {
+            if(l==1)
+            {
+                continue;
+            }
+            cout<<l<<" ";
+        }
+        cout<<endl;
+    }
+}
+
+.....................................another way ...........................................
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int row;
+    cout<<"row: ";
+    cin>>row;
+    for(int i=1; i<=row; i++)
+    {
+        int j;
+        for(j=1; j<=row-i; j++)
+        {
+            cout<<"  ";
+        }
+        int k =i;
+        for(;j<=row;j++)
+        {
+            cout<<k--<<" ";
+        }
+        k=2;
+        for(;j<=row+i-1;j++)
+        {
+            cout<<k++<<" ";
+        }
+        cout<<endl;
+    }
+}
