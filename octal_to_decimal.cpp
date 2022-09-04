09b721c91aa65253c3bdc172a78c95b47e827a82
Sh Tanmoy <62763494+tanmoy108@users.noreply.github.com>
2022-09-04 13:51:38 +0600

    octal_to_decimal.cpp

---
+++ octal_to_decimal.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int oct,last=0,i=0,sum=0;
+    cout<<"enter octal number: ";
+    cin>>oct;
+
+    while(oct>0)
+    {
+        last = oct%10;
+        last = last * pow(8,i);
+        sum+=last;
+        ++i;
+        oct=oct/10;
+    }
+    cout<<"decimal number: "<<sum;
+}
