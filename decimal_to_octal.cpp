f45f3394fc0e4c6e0bb03647b9a945e9bff7d374
Sh Tanmoy <62763494+tanmoy108@users.noreply.github.com>
2022-09-04 17:10:34 +0600

    decimal_to_octal.cpp

---
+++ decimal_to_octal.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int dec,arr[1000],i=0;
+    cin>>dec;
+
+    while(dec!=0){
+        arr[i]=dec%8;
+        dec=dec/8;
+        ++i;
+    }
+    for(int j=i-1;j>=0;j--)
+    {
+        cout<<arr[j];
+    }
+}
