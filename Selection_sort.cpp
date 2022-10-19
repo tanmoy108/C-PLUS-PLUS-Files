eb6dbf1bc4dc5a198675f7c9ba8780b1c6561502
Sh Tanmoy <62763494+tanmoy108@users.noreply.github.com>
2022-10-19 14:57:29 +0600

    Create Selection_sort.cpp
    Selection sort

---
+++ Selection_sort.cpp
@@ -0,0 +1,27 @@
+//Selection sort
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int arr[]={16,11,23,22,51,34,73,2};
+    int n = (sizeof(arr)/sizeof(arr[0]));
+    int temp;
+    for(int i=0;i<n;i++)
+    {
+        for(int j=i+1;j<n;j++)
+        {
+            if(arr[i]>arr[j])
+            {
+                temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+    for(int i:arr)
+    {
+        cout<<i<<" ";
+    }
+
+}
