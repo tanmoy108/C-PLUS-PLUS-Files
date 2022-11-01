2cc9dc3384b6530df1351f23cb66aed4bb0e165d
Sh Tanmoy <62763494+tanmoy108@users.noreply.github.com>
2022-11-02 01:50:30 +0600

    binary_sort.cpp
    binary_sort

---
+++ binary_sort.cpp
@@ -0,0 +1,27 @@
+//binary sort
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int arr[]={12,45,23,51,19,8};
+    int n = (sizeof(arr)/sizeof(arr[0]));
+    int temp;
+    for(int i=0;i<n;i++)
+    {
+        for(int i=0,j=i+1 ; i<n-1; i++,j++)
+    {
+        if(arr[i]>arr[j])
+        {
+            temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+    }
+    }
+    for(int i:arr)
+    {
+        cout<<i<<" ";
+    }
+
+}
