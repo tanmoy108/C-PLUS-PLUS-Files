20bc30f0426ed912bbc542bdf42308827485b340
Sh Tanmoy <62763494+tanmoy108@users.noreply.github.com>
2023-01-10 15:18:59 +0600

    1. hollow_rectangle_pattern.cpp (pattern)

---
+++ 1_hollow_rectangle_pattern.cpp
@@ -0,0 +1,34 @@
+// 1. Hollow Rectangle Pattern
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int row = 8, col = 4;
+    for(int i=0;i<row;i++)
+    {
+        for(int j=0;j<row;j++)
+        {
+            if(i==0 || i==row-1)
+            {
+                cout<<"*";
+            }
+            else
+            {
+                if(j==0 || j==row-1)
+                {
+                    cout<<"*";
+                }
+                else
+                {
+                    cout<<" ";
+                }
+            }
+        }
+        cout<<endl;
+    }
+
+    return 0;
+}
