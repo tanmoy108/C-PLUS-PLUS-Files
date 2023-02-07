b31a05f345c6a22c623981680aba2587678ef0a0
Sh Tanmoy <62763494+tanmoy108@users.noreply.github.com>
2023-02-07 13:19:58 +0600

    57_bit_manipulation_problem_3.cpp (problem 3)

---
+++ 57_bit_manipulation_problem_2.cpp
@@ -0,0 +1,13 @@
+//clear bit . clear bit means set 0 on ith position
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n = 5; // 0101
+    int i = 2;
+    int a = n&(~(1<<i));// 0101 & 1011  <=(~0100)
+    cout<<a; // 1=0001
+}
+
